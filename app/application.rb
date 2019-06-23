951a8c7056cc35c1c7e12965646cf5a75c31a05f
Kevin <kevinlai76@gmail.com>
2019-06-23 18:57:51 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,36 @@
+class Application
+  @@items = [Item.new("Figs", 3.42),
+    Item.new("Pears", .99)]
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end 
+    
+    resp.finish
+  end 
+  
+end
+
+
+ def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/songs"
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
+end
+ 
