c335ca58a14b96f7abc195b982d19a5fadc169d2
Kevin <kevinlai76@gmail.com>
2019-06-23 19:23:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,14 +15,13 @@ class Application
           resp.write "Item not found"
           resp.status = 400
         end
+      end 
     else
       resp.write "Route not found"
       resp.status = 404
     end 
     
     resp.finish
-  end 
+  end
   
 end
-
- 
