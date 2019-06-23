57fb93e7972f39a9c89c6b340f278c89b5721c5d
Kevin <kevinlai76@gmail.com>
2019-06-23 19:06:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find { |i| i.name = item_name }
       
-      resp.write Item.
+      resp.write Item.price
     else
       resp.write "Route not found"
       resp.status = 404
