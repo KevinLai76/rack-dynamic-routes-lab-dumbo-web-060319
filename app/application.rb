10a1778ca74849fb03d332b2e5ddab72650c7a9c
Kevin <kevinlai76@gmail.com>
2019-06-23 19:38:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find do |i| 
         if i.name = item_name 
-          resp.write Item.price
+          resp.write "#{item.price}"
         else 
           resp.write "Item not found"
           resp.status = 400
