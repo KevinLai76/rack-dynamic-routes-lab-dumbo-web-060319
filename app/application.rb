5cdf11ca3f045f74dec629405655c75e83b75735
Kevin <kevinlai76@gmail.com>
2019-06-23 20:09:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,11 +9,11 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find { |i| i.name = item_name }
-      
-      resp.write "#{item.price}"
-    elsif 
-      resp.write "Item not found"
-      resp.status = 400
+        if item == item_name
+          resp.write "#{item.price}"
+        else
+          resp.write "Item not found"
+          resp.status = 400
     else
       resp.write "Route not found"
       resp.status = 404
